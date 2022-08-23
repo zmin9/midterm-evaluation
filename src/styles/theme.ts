c134af6ed52e7f0a0b92a71b96c885fc68dcdc40
o3o0903@gmail.com <o3o0903@gmail.com>
2022-08-23 11:57:56 +0900

    feat: add palette.ts and theme.ts

---
+++ src/styles/theme.ts
@@ -0,0 +1,17 @@
+import palette from './palette';
+
+const theme = {
+  bg1: palette.white,
+  bg2: palette.gray1,
+
+  headline: palette.black,
+  text1: palette.gray4,
+  text2: palette.gray3,
+  text3: palette.gray2,
+  highlight: palette.purple2,
+
+  button: palette.purple2,
+  onButton: palette.white,
+};
+
+export default theme;
