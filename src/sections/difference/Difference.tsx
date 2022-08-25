1b101716c8300cfe296a79d88cacffc66e84d1ca
o3o0903@gmail.com <o3o0903@gmail.com>
2022-08-25 10:51:59 +0900

    feat: add production.ts

--- src/sections/difference/Difference.tsx
+++ src/sections/difference/Difference.tsx
@@ -4,7 +4,7 @@ import mediaQuery from '../../styles/mediaQuery';
 import differenceWithOthers from '../../data/differenceWithOthers';
 import QuestionBox from '../../components/QuestionBox';
 import Text, { Highlight } from '../../components/Text';
-
+import production, { Production } from '../../data/production';
 
 const GraphImage = styled.img`
   width: 100%;
@@ -16,10 +16,95 @@ const GraphImage = styled.img`
   }
 `;
 
+const ProductionItem = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+
+  ${mediaQuery.large} {
+    flex-direction: column-reverse;
+    align-items: start;
+  }
+
+  & + & {
+    margin-top: 24px;
+
+    ${mediaQuery.large} {
+      margin-top: 0;
+    }
+  }
+`;
+
+const ItemText = styled.div`
+  display: flex;
+  flex-direction: column;
+
+  & > * + * {
+    margin-top: 8px;
+  }
+
+  ${mediaQuery.large} {
+    margin-top: 16px;
+  }
+`;
+const Productions = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin-right: 10px;
+
+  ${mediaQuery.large} {
+    flex-direction: row;
+    justify-content: space-between;
+    margin-right: -56px;
+
+    & > * {
+      flex: 1 1 0;
+    }
+  }
+`;
+
+const ItemTitle = styled.span`
+  color: ${({ theme }) => theme.text1};
+`;
+
+const ItemDesc = styled.span`
+  white-space: pre-wrap;
+  color: ${({ theme }) => theme.text2};
+`;
+
+const ItemImgContainer = styled.div<Pick<Production, 'imgBgColor'>>`
+  width: 64px;
+  height: 64px;
+  background-color: ${({ imgBgColor }) => imgBgColor};
+  border-radius: 12px;
+  padding: 0 14px;
+
+  display: flex;
+  align-items: center;
+
+  img {
+    width: 100%;
+  }
+`;
+
+const ProductionText = styled.h3`
+  text-align: center;
+  color: ${({ theme }) => theme.text2};
+  margin-top: 60px;
+
+  & > * {
+    display: block;
+
+    ${mediaQuery.large} {
+      display: inline;
+    }
+  }
+`;
+
 const Difference = () => {
   return (
     <>
-      <Section bgColor='bg2' paddingT={60}>
+      <Section bgColor="bg2" paddingT={60}>
         <QuestionBox>
           <Text type="H1">수학대왕 <Highlight>프리미엄</Highlight>{'은 '}</Text>
           <Text type="H1">무엇이 다른가요?</Text>
@@ -35,6 +120,39 @@ const Difference = () => {
           </Section.Card>
         </Section>
       ))}
+      <Section bgColor="bg1" paddingT={60} paddingB={80}>
+        <Productions>
+          {
+            production.map((item) => (
+              <ProductionItem key={item.title}>
+                <Text type="textSR">
+                  <ItemText>
+                    <ItemTitle>
+                      {item.title}
+                    </ItemTitle>
+                    <ItemDesc>
+                      {item.description}
+                    </ItemDesc>
+                  </ItemText>
+                </Text>
+                <ItemImgContainer imgBgColor={item.imgBgColor}>
+                  <img src={item.img} alt={item.title + '_icon'}/>
+                </ItemImgContainer>
+              </ProductionItem>
+            ))
+          }
+        </Productions>
+        <ProductionText>
+          <Text type="textMB">
+            {'이 모든 구성이 수학대왕 '}
+            <Highlight>프리미엄</Highlight>
+            {'에 '}
+          </Text>
+          <Text type="textMB">
+            포함되어 있습니다.
+          </Text>
+        </ProductionText>
+      </Section>
     </>
   );
 };
