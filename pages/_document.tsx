8a8164bbf3ba6bebd9341997fc240ad4f0de2604
o3o0903@gmail.com <o3o0903@gmail.com>
2022-08-28 10:04:12 +0900

    fix: remove prefix for public folder in _document.tsx

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,22 +1,20 @@
 import Document, { Head, Html, Main, NextScript } from 'next/document';
 
-const prefix = process.env.NODE_ENV !== 'production' ? '' : process.env.NEXT_PUBLIC_PREFIX;
-
 class MyDocument extends Document {
   render() {
     return (
       <Html lang="ko">
         <Head>
           <meta charSet="utf-8"/>
-          <link rel="icon" href={`${prefix}/images/mathking_logo.png`}/>
+          <link rel="icon" href='/images/mathking_logo.png'/>
           <meta name="theme-color" content="#835EEB"/>
           <meta
             name="description"
             content="이제는 수학도 인공지능! 인공지능이 분석해주는 수학 실력과 나에게 꼭 필요한 문제만 풀고 싶다면 지금 바로 수학대왕으로! AI와 함께하는 수학공부는 수학대왕!"
           />
           <meta name="keywords" content="인공지능, AI, 수학, 학습"/>
-          <link rel="apple-touch-icon" href={`${prefix}/images/mathking_logo.png`}/>
-          <link rel="manifest" href={`${prefix}/manifest.json`}/>
+          <link rel="apple-touch-icon" href='/images/mathking_logo.png'/>
+          <link rel="manifest" href='/manifest.json'/>
           <link rel="stylesheet" as="style" crossOrigin="anonymous"
                 href="https://cdn.jsdelivr.net/gh/orioncactus/pretendard@v1.3.5/dist/web/variable/pretendardvariable.css"/>
           <link rel="canonical" href="https://zmin9.github.io/premium-page/"/>
