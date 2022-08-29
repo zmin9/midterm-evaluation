cf1dd92e8e9daa82bcc505ae9cc3b4db1c0e7b9a
o3o0903@gmail.com <o3o0903@gmail.com>
2022-08-29 11:47:25 +0900

    refactor: combine response button into PremiunTalk section

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,32 +6,6 @@ import Question2 from '../src/sections/question2/Question2';
 import Question3 from '../src/sections/question3/Question3';
 import PremiumTalk from '../src/sections/PremiumTalk';
 import Footer from '../src/sections/Footer';
-import mediaQuery from '../src/styles/mediaQuery';
-import typography from '../src/styles/typography';
-
-const ResponseButton = styled.button`
-  background-color: ${({ theme }) => theme.button};
-  color: ${({ theme }) => theme.onButton};
-  ${typography.textMB}
-
-  position: sticky;
-  bottom: 20px;
-  display: block;
-  margin: 0 auto 40px;
-
-  border-radius: 9999px;
-  border: none;
-  padding: 12px 0;
-
-  width: calc(100% - 40px);
-  max-width: 732px;
-
-
-  ${mediaQuery.large} {
-    ${typography.H3}
-    padding: 15px 0;
-  }
-`;
 
 const Background = styled.div`
   background-color: ${({ theme }) => theme.bg2};
@@ -46,9 +20,6 @@ const Index = () => {
       <Question2/>
       <Question3/>
       <PremiumTalk/>
-      <ResponseButton onClick={() => alert('결제 성공')}>
-        자녀분의 요청에 응답하기
-      </ResponseButton>
       <Footer/>
     </Background>
   );
