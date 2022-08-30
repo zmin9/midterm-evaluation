5b36d10d1448f38524d364d6020f0f79fb9cc189
zmin9 <60884877+zmin9@users.noreply.github.com>
2022-08-30 16:46:08 +0000

    Deploying to gh-pages from @ zmin9/premium-page@e92b6ed104f7431304c35b458a4ed4b5e70f6083 🚀

--- _next/static/FKMGvjbtj5L-lRp-QsH6h/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/98-8e56553bbee7d0ec.js","static/chunks/pages/index-2d0e1bd50c02a556.js"],"/_error":["static/chunks/pages/_error-6384aa500e86e64a.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
